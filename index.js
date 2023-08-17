103fb1ec1346808923f2d8b74e12752496421511
Soniya <soniya19106208@gmail.com>
2023-08-17 20:18:52 +0530

    Updated code

---
+++ index.js
@@ -0,0 +1,23 @@
+// const images = document.querySelectorAll('.image');
+
+// images.forEach(image => {
+//     image.addEventListener('click', () => {
+//         const imgSrc = image.querySelector('img').src;
+//         showLightbox(imgSrc);
+//     });
+// });
+
+// function showLightbox(imgSrc) {
+//     const lightbox = document.createElement('div');
+//     lightbox.classList.add('lightbox');
+
+//     const img = document.createElement('img');
+//     img.src = imgSrc;
+//     lightbox.appendChild(img);
+
+//     lightbox.addEventListener('click', () => {
+//         lightbox.remove();
+//     });
+
+//     document.body.appendChild(lightbox);
+// }
